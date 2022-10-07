601637c8a795115309b06f379e9d2c42bbf64b05
Roman <romantovt31@gmail.com>
2022-10-07 21:44:51 +0300

    Code Style: test_main.py

---
+++ django_contexter/models/test/test_views/test_calls.py
@@ -0,0 +1,79 @@
+"""Contains "call" tests."""
+
+from django.contrib.auth.models import Permission
+from django.test import RequestFactory, TestCase
+from rest_framework import status
+
+from django_contexter.models.errors.err_codes import NO_MANDATORY_PARAMETER_MODELNAME
+from django_contexter.models.serializer import Serializer
+from django_contexter.models.test.test_views.configs import Configs
+from django_contexter.models.views import index
+
+
+class CallsTestCase(TestCase, Configs):
+    """Part of ViewsMainTestCase: contains all "call" tests."""
+
+    def setUp(self):  # noqa: D102
+        self.factory = RequestFactory()
+
+    def test_call_without_model_name(self):
+        """Test call without modelName, expect error."""
+        response = self._get_response("".join(["/api/models/"]))
+
+        self.assertEqual(response.data["err_code"], NO_MANDATORY_PARAMETER_MODELNAME)
+
+    def test_call_with_model_name(self):
+        """Test call with modelName, expect all records."""
+        with self.settings(
+            CONTEXTER_ACCESS_POLICY=self.configs["test_call_with_modelName"],
+        ):
+            response = self._get_response(
+                "".join(["/api/models/", "?modelName=auth.Permission"]),
+            )
+            model_objects = Permission.objects
+            serialized = Serializer(
+                model_objects, many=True, context={"model": Permission, "fields": "__all__"},
+            )
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.data, serialized.data)
+
+    def test_all_call(self):
+        """Test "all" call, expect all records."""
+        with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_all_call"]):
+            response = self._get_response(
+                "".join(["/api/models/", "?modelName=auth.Permission", "&all"]),
+            )
+            model_objects = Permission.objects.all()
+            serialized = Serializer(
+                model_objects, many=True, context={"model": Permission, "fields": "__all__"},
+            )
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.data, serialized.data)
+
+    def test_get_call(self):
+        """Test "get" call, expect ONLY ONE records."""
+        with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_get_call"]):
+            response = self._get_response(
+                "".join(
+                    [
+                        "/api/models/",
+                        "?modelName=auth.Permission",
+                        "&all",
+                        '&get={"pk": 1}',
+                    ]
+                )
+            )
+            model_objects = Permission.objects.all().get(pk=1)
+            serialized = Serializer(
+                model_objects, many=False, context={"model": Permission, "fields": "__all__"}
+            )
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(response.data, serialized.data)
+
+    def _get_response(self, path):
+        request = self.factory.get(path)
+
+        return index(request)
--- django_contexter/models/test/test_views/test_main.py
+++ django_contexter/models/test/test_views/test_main.py
@@ -1,149 +1,67 @@
-from django.conf import settings
-from django.test import RequestFactory, TestCase
-from django_contexter.models.change_result import ChangeResult
-
-from django_contexter.models.method_types import ALL_METHODS, ALL_SAFE_METHODS
-from django_contexter.models.test.test_views.configs import Configs
-
-from ...errors.err_codes import (
-    FIELD_ERROR,
-    FUNCTION_DOES_NOT_EXIST_IN_QUERYSET_API,
-    NO_MANDATORY_PARAMETER_MODELNAME,
-    REJECT_ERROR,
-    SERVER_WRONG_CONFIG,
-)
-
-from ...errors.request_error import RequestError
-from ...views import Error_Handler, exclude_keys, index
-
-from ...errors.configuration_error import ConfigurationError
-
-from ...errors.reject_error import RejectError
+"""Test index function from views.py."""
 
 from django.contrib.auth.models import Permission
-from ...serializer import Serializer
+from django.test import RequestFactory, TestCase
+from rest_framework import status
+
+from django_contexter.models.change_result import ChangeResult
+from django_contexter.models.errors.err_codes import FIELD_ERROR, FUNCTION_DOES_NOT_EXIST_IN_QUERYSET_API, REJECT_ERROR
+from django_contexter.models.method_types import ALL_METHODS, ALL_SAFE_METHODS
+from django_contexter.models.serializer import Serializer
+from django_contexter.models.test.test_views.configs import Configs
+from django_contexter.models.views import index
 
 
-class ViewsMainTestCase(TestCase, Configs):
-    def setUp(self):
+class MainTestCase(TestCase, Configs):
+    """Test index function from views.py."""
+
+    def setUp(self):  # noqa: D102
         self.factory = RequestFactory()
 
-    def test_call_without_modelName(self):
-        request = self.factory.get("".join(["/api/models/"]))
-        response = index(request)
-
-        self.assertEqual(response.data["err_code"], NO_MANDATORY_PARAMETER_MODELNAME)
-
-    def test_call_with_modelName(self):
-        with self.settings(
-            CONTEXTER_ACCESS_POLICY=self.configs["test_call_with_modelName"]
-        ):
-            request = self.factory.get(
-                "".join(["/api/models/", "?modelName=auth.Permission"])
-            )
-            objects = Permission.objects
-            serialized = Serializer(
-                objects, many=True, context={"model": Permission, "fields": "__all__"}
-            )
-            response = index(request)
-
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data, serialized.data)
-
-    def test_all_call(self):
-        with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_all_call"]):
-            request = self.factory.get(
-                "".join(["/api/models/", "?modelName=auth.Permission", "&all"])
-            )
-            objects = Permission.objects.all()
-            serialized = Serializer(
-                objects, many=True, context={"model": Permission, "fields": "__all__"}
-            )
-            response = index(request)
-
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data, serialized.data)
-
-    def test_get_call(self):
-        with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_get_call"]):
-            request = self.factory.get(
-                "".join(
-                    [
-                        "/api/models/",
-                        "?modelName=auth.Permission",
-                        "&all",
-                        '&get={"pk": 1}',
-                    ]
-                )
-            )
-            objects = Permission.objects.all().get(pk=1)
-            serialized = Serializer(
-                objects, many=False, context={"model": Permission, "fields": "__all__"}
-            )
-            response = index(request)
-
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data, serialized.data)
-
     def test_check_method(self):
+        """Test check_method function."""
         with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_check_method"]):
             for method in ALL_METHODS - ALL_SAFE_METHODS:
-                request = self.factory.get(
-                    "".join(
-                        ["/api/models/", "?modelName=auth.Permission", f"&{method}"]
-                    )
-                )
-                response = index(request)
+                response = self._get_response("".join([
+                    "/api/models/", "?modelName=auth.Permission", f"&{method}",
+                ]))
 
-                self.assertEqual(response.status_code, 403)
+                self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
                 self.assertEqual(response.data["err_code"], REJECT_ERROR)
 
     def test_field_error(self):
+        """Test FieldError handling."""
         with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_field_error"]):
-            request = self.factory.get(
-                "".join(
-                    [
-                        "/api/models/",
-                        "?modelName=auth.Permission",
-                        "&all",
-                        '&get={"pk": 9999}',
-                    ]
-                )
-            )
-            response = index(request)
+            response = self._get_response("".join([
+                "/api/models/",
+                "?modelName=auth.Permission",
+                "&all",
+                '&get={"pk": 9999}',
+            ]))
 
-            self.assertEqual(response.status_code, 400)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
             self.assertEqual(response.data["err_code"], FIELD_ERROR)
 
     def test_attr_error(self):
+        """Test AttributeError handling."""
         with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_attr_error"]):
-            request = self.factory.get(
-                "".join(
-                    [
-                        "/api/models/",
-                        "?modelName=auth.Permission",
-                        "&all",
-                        "&commandNotExists",
-                    ]
-                )
-            )
-            response = index(request)
+            response = self._get_response("".join([
+                "/api/models/",
+                "?modelName=auth.Permission",
+                "&all",
+                "&commandNotExists",
+            ]))
 
-            self.assertEqual(response.status_code, 400)
-            self.assertEqual(
-                response.data["err_code"], FUNCTION_DOES_NOT_EXIST_IN_QUERYSET_API
-            )
-            self.assertEqual(
-                response.data["err_msg"],
-                "No function named 'commandNotExists' in QuerySet API",
-            )
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(response.data["err_code"], FUNCTION_DOES_NOT_EXIST_IN_QUERYSET_API)
+            self.assertTrue("commandnotexists" in response.data["err_msg"].lower())
 
     def test_hide(self):
+        """Test hiding feature."""
         with self.settings(CONTEXTER_ACCESS_POLICY=self.configs["test_hide"]):
-            request = self.factory.get(
-                "".join(["/api/models/", "?modelName=auth.Permission", "&all"])
-            )
-            response = index(request)
+            response = self._get_response("".join([
+                "/api/models/", "?modelName=auth.Permission", "&all",
+            ]))
 
             changer = ChangeResult(
                 {"hidden_fields": ["codename"], "allow_methods": ALL_METHODS},
@@ -159,5 +77,10 @@ class ViewsMainTestCase(TestCase, Configs):
                 context={"model": Permission, "fields": "__all__"},
             )
 
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
             self.assertEqual(response.data, serialized.data)
+
+    def _get_response(self, path):
+        request = self.factory.get(path)
+
+        return index(request)
