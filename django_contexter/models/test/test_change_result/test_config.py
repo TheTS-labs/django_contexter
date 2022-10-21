b97a329694d5d06c61e637d8faa030ecae90dfae
Roman <romantovt31@gmail.com>
2022-10-21 14:47:43 +0300

    Fix some mistakes in tests

--- django_contexter/models/test/test_change_result/test_config.py
+++ django_contexter/models/test/test_change_result/test_config.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import Permission
 from django.test import TestCase
 
 from django_contexter.models.change_result import ChangeResult
-from django_contexter.models.method_types import ALL_SAFE_METHODS
 
 
 class ChangeResultConfigTestCase(TestCase):
@@ -17,13 +16,3 @@ class ChangeResultConfigTestCase(TestCase):
         """Except no changes in records with None config."""
         changer = ChangeResult(None, Permission, None)
         self.assertEqual(changer.fix_fields(Permission.objects), Permission.objects)
-
-    # def test_no_changes_in_simple_and_extended(self):
-    #     """Except no changes in record if field in hidden_fields AND recorded extended."""
-    #     changer = ChangeResult(
-    #         {"allow_methods": ALL_SAFE_METHODS, "hidden_fields": {"codename": ""}},
-    #         Permission,
-    #         None,
-    #     )
-
-    #     self.assertEqual(changer.fix_fields(Permission.objects), Permission.objects)
